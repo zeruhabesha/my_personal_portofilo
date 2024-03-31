f8fdb3e050aa52e5b7adc4b537ee19a2aa49046f
zeruhabesha <88440051+zeruhabesha@users.noreply.github.com>
2024-03-31 05:59:42 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,3 +36,21 @@ $(document).ready(function(){
   });
 
 });
+
+
+
+
+
+// Function to open the popup
+function openPopup(imgSrc) {
+  var popup = document.getElementById('popup');
+  var popupImg = document.getElementById('popup-img');
+  popupImg.src = imgSrc;
+  popup.style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+  var popup = document.getElementById('popup');
+  popup.style.display = 'none';
+}
